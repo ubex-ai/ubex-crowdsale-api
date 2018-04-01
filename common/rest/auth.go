bb1c4c7ed6eccc44e3ec8542c02e3436db5d71dc
Dan Gartman <dan.gartman@hotmail.com>
2018-04-01 20:15:38 +0100

    common rest package

---
+++ common/rest/auth.go
@@ -0,0 +1,33 @@
+package rest
+
+import (
+    "github.com/gin-gonic/gin"
+    "net/http"
+    "github.com/spf13/viper"
+    "fmt"
+)
+
+const SIGN_HEADER = "X-Authorization"
+
+func SignRequired() gin.HandlerFunc {
+    return func(c *gin.Context) {
+        signHeader := c.GetHeader(SIGN_HEADER)
+        if signHeader == "" {
+            NewResponder(c).ErrorAuthorization()
+            return
+        }
+
+        if signHeader != viper.GetString("auth.secret") {
+            NewResponder(c).ErrorWithCode(
+                http.StatusUnauthorized,
+                ErrorAuthorization,
+                fmt.Sprintf(
+                    "Incorrect signature, provided %v, expected %v",
+                    signHeader,
+                    viper.GetString("auth.secret"),
+                ),
+            )
+            return
+        }
+    }
+}
