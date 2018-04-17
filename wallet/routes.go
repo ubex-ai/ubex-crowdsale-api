85d8975345a7de118eedd2d5ced57e8d1a6e85d2
Dan Gartman <dan.gartman@hotmail.com>
2018-04-17 22:14:32 +0100

    wallet package

---
+++ wallet/routes.go
@@ -0,0 +1,81 @@
+package wallet
+
+import (
+    "github.com/gin-gonic/gin"
+    "ubex-api/common/rest"
+    "ubex-api/common/ethereum"
+    "ubex-api/models"
+)
+
+func InitRoutes(router *gin.Engine) {
+    eth := router.Group("/wallet")
+    {
+        eth.GET("/balance/:address", getEthBalanceAction)
+        eth.POST("/balances", getEthBalancesAction)
+    }
+}
+
+// swagger:route GET /wallet/balance/:address ethereum getEthBalance
+//
+// Get ETH balance
+//
+// Get particular Ethereum wallet balance.
+//
+// Consumes:
+// - application/json
+// Produces:
+// - application/json
+// Responses:
+//   200: GetBalanceSuccessResponse
+//   400: RestErrorResponse
+//
+func getEthBalanceAction(c *gin.Context) {
+    addr := c.Param("address")
+    bal, err := ethereum.GetWallet().BalanceAt(addr)
+    if err != nil {
+        rest.NewResponder(c).Error(err.Error())
+        return
+    }
+
+    rest.NewResponder(c).Success(gin.H{
+        "address": addr,
+        "balance": bal.String(),
+    })
+}
+
+// swagger:route POST /wallet/balances ethereum getEthBalances
+//
+// Get a list of balances for Ethereum wallets
+//
+// Get balances for list of Ethereum addresses.
+//
+// Consumes:
+// - application/json
+// Produces:
+// - application/json
+// Responses:
+//   200: GetBalancesSuccessResponse
+//   400: RestErrorResponse
+//
+func getEthBalancesAction(c *gin.Context) {
+    request := &models.Addresses{}
+    err := c.BindJSON(request)
+    if err != nil {
+        rest.NewResponder(c).ErrorValidation(err.Error())
+        return
+    }
+
+    bals := map[string]string{}
+    for _, addr := range request.Addresses {
+        bal, err := ethereum.GetWallet().BalanceAt(addr)
+        if err != nil {
+            rest.NewResponder(c).Error(err.Error())
+            return
+        }
+        bals[addr] = bal.String()
+    }
+
+    rest.NewResponder(c).Success(gin.H{
+        "balances": bals,
+    })
+}
