c1c1b3d448de09d78d17342e71c54a8b88276949
Dan Gartman <dan.gartman@hotmail.com>
2018-05-02 22:13:34 +0100

    crowdsale models

---
+++ models/crowdsale.go
@@ -0,0 +1,27 @@
+package models
+
+type CrowdsaleDeployParams struct {
+    WalletAddress string `json:"wallet"`
+    TokenRate string `json:"rate"`
+}
+
+type CrowdsaleStatus struct {
+    Address string `json:"address"`
+    Rate string `json:"rate"`
+    WeiRaised string `json:"weiRaised"`
+    TokensIssued string `json:"tokensIssued"`
+}
+
+type TokenPaidEventArgs struct {
+    Purchaser string `json:"purchaser"`
+    Beneficiary string `json:"beneficiary"`
+    WeiAmount string `json:"weiAmount"`
+    Created string `json:"created"`
+}
+
+type TokenPurchaseEventArgs struct {
+    Purchaser string `json:"purchaser"`
+    Beneficiary string `json:"beneficiary"`
+    WeiAmount string `json:"weiAmount"`
+    TokensAmount string `json:"tokensAmount"`
+}
